badd74022a093d896bb80b9566f5bfa74d457bdb
sweetrain096 <gpfhddl09@gmail.com>
2018-10-12 17:07:54 +0000

    로컬호스트에 연결 10/13

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
-hello IoT 
-너무너 무어렵다 ㅠㅠ
+var express = require('express');
+
+var app = express();
+var port = 5000;
+
+app.get('/', function (req, res) {
+	console.log("Get request");
+	res.send('10/13일 Hello World!');
+})
+
+app.listen(port, function() {
+	console.log('Raspberry pi server is listening on port' + port);
+})
